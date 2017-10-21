55e0ef273ddbffddecea68eba95fdd8e9416b6d2
Mark Philipp <Wolfsenshi@gmail.com>
2017-10-20 21:25:40 -0700

    Deletion is Working

--- Lab5/Prog5.cpp
+++ Lab5/Prog5.cpp
@@ -32,7 +32,7 @@ public:
 
 	int GetName()
 	{
-return name;
+		return name;
 	}
 
 	int GetKey()
@@ -45,37 +45,52 @@ return name;
 		key = newKey;
 	}
 
-int GetNext()
-{
-	return next;
-}
+	int GetNext()
+	{
+		return next;
+	}
 
-int GetPrevious()
-{
-	return previous;
-}
+	int GetPrevious()
+	{
+		return previous;
+	}
 
-bool GetMark()
-{
-	return mark;
-}
+	bool GetMark()
+	{
+		return mark;
+	}
 
-int SetNext(int newVal)
-{
-	next = newVal;
-}
+	int SetNext(int newVal)
+	{
+		next = newVal;
+	}
 
-void Insertion(int newKey, int newNext, int newPrev)
-{
-	// called when insertion happens. Updates variables appropriately.
-	key = newKey;
-	next = newNext;
-	previous = newPrev;
-}
+	int SetPrevious(int newVal)
+	{
+		previous = newVal;
+	}
 
+	void Insertion(int newKey, int newNext, int newPrev)
+	{
+		// called when insertion happens. Updates variables appropriately.
+		key = newKey;
+		next = newNext;
+		previous = newPrev;
+	}
+
+	void Deletion()
+	{
+		// called when a delete happens. Removes previous node
+		previous = -1;
+	}
+
+	/*bool operator== (Node& rhs)
+	{
+		return (this->GetKey() == rhs.GetKey());
+	}*/
 };
 
-vector<Node> freeList, L1, L2;
+vector<Node> freeList, L1, L2, allNodes; // allNodes will be used to check for garbage nodes. will compare all lists to allNodes. If we don't find a node from allNodes in any of the other lists, then we know it's garbage
 
 void InitializeNodes()
 {
@@ -85,19 +100,33 @@ void InitializeNodes()
 		{
 			Node newNode(i, 0, i + 1, -1, 0);
 			freeList.push_back(newNode);
+			allNodes.push_back(newNode);
 		}
 		else if (i < 10) // handle node creation between 1 and 10
 		{
 			Node newNode(i, 0, i + 1, i - 1, 0);
 			freeList.push_back(newNode);
+			allNodes.push_back(newNode);
 		}
 		else // handle 10th node creation
 		{
 			Node newNode(i, 0, -1, i - 1, 0);
 			freeList.push_back(newNode);
+			allNodes.push_back(newNode);
 		}
 
 		//cout << freeList.at(i - 1).GetName() << "\t" << freeList.at(i-1).GetKey() << "\t" << freeList.at(i-1).GetNext() << "\t" << freeList.at(i-1).GetMark() << endl;
+	}
+}
+
+Node* GetNode(Node& nodeToFind)
+{
+	// This function is used for finding a node in the allNodes list that is equal to the node we're looking for.
+	// Primarily will be used to update nodes in the allNodes list when inserting and deleting happens
+	for (auto& node : allNodes)
+	{
+		if (node.GetName() == nodeToFind.GetName())
+			return &node;
 	}
 }
 
@@ -117,17 +146,60 @@ void Insert(vector<Node> &nodeList, int key)
 	nodeList.push_back(freeList.at(0));
 	freeList.erase(freeList.begin());
 
-	if (nodeList.size() == 0)
+	if (nodeList.size() == 1)
+	{
 		nodeList.back().Insertion(key, -1, -1);
-	else if (nodeList.size() > 1 && nodeList.size() < 10)
-	{
-		nodeList.back().Insertion(key, -1, nodeList.at(nodeList.size() - 2).GetName());
-		nodeList.at(nodeList.size() - 2).SetNext(nodeList.back().GetName());
+		GetNode(nodeList.back())->Insertion(key, -1, -1);
 	}
-	else if (nodeList.size() > 1 && nodeList.size() == 10)
+	else if (nodeList.size() > 1 && nodeList.size() <= 10)
 	{
 		nodeList.back().Insertion(key, -1, nodeList.at(nodeList.size() - 2).GetName());
 		nodeList.at(nodeList.size() - 2).SetNext(nodeList.back().GetName());
+
+		GetNode(nodeList.back())->Insertion(key, -1, nodeList.at(nodeList.size() - 2).GetName());
+		GetNode(nodeList.at(nodeList.size() - 2))->SetNext(nodeList.back().GetName());
+	}
+}
+
+void Delete(vector<Node> &nodeList, int key)
+{
+	/*if (freeList.size() == 0)
+	{
+		GarbageCollect();
+		return;
+	}*/
+
+	for (int i = 0; i < nodeList.size(); i++)
+	{
+		if (nodeList.at(i).GetKey() == key)
+		{
+			// found node to remove
+			nodeList.at(i).Deletion();
+			GetNode(nodeList.at(i))->Deletion();
+
+			if (i == 0 && nodeList.size() > 1)
+			{
+				nodeList.at(i + 1).SetPrevious(-1);
+				GetNode(nodeList.at(i + 1))->SetPrevious(-1);
+			}
+			else if (i > 0 && i < nodeList.size() - 1)
+			{
+				// set new next and previous nodes for the nodes before and after deleted node.
+				nodeList.at(i - 1).SetNext(nodeList.at(i + 1).GetName());
+				nodeList.at(i + 1).SetPrevious(nodeList.at(i - 1).GetName());
+
+				GetNode(nodeList.at(i - 1))->SetNext(nodeList.at(i + 1).GetName());
+				GetNode(nodeList.at(i + 1))->SetPrevious(nodeList.at(i - 1).GetName());
+			}
+			else if (i == nodeList.size() - 1 && nodeList.size() > 1)
+			{
+				// case for deleting last node in the list
+				nodeList.at(i - 1).SetNext(-1);
+				GetNode(nodeList.at(i - 1))->SetNext(-1);
+			}
+
+			nodeList.erase(nodeList.begin() + i);
+		}
 	}
 }
 
@@ -135,7 +207,26 @@ void PrintMemory()
 {
 	cout << "\t";
 
-	for (int i = 1; i <= 10; i++)
+	for (auto& node : allNodes)
+		cout << node.GetName() << "\t";
+
+	cout << endl << "Key\t";
+
+	for (auto& node : allNodes)
+		cout << node.GetKey() << "\t";
+
+	cout << endl << "Next\t";
+
+	for (auto& node : allNodes)
+		cout << node.GetNext() << "\t";
+
+	cout << endl << "Mark\t";
+
+	for (auto& node : allNodes)
+		cout << node.GetMark() << "\t";
+
+	cout << endl;
+	/*for (int i = 1; i <= 10; i++)
 	{
 		cout << i << "\t";
 	}
@@ -248,7 +339,7 @@ void PrintMemory()
 		}
 	}
 
-	cout << endl;
+	cout << endl;*/
 }
 
 
@@ -264,6 +355,11 @@ int main(int argc, const char **argv)
 	Insert(L2, 8);
 	Insert(L1, 4);
 
+	Delete (L1, 4);
+	Delete (L2, 8);
+	Delete (L1, 1);
+	Delete (L2, 4);
+	Delete (L1, 5);
 	/*for (auto& i : freeList)
 		cout << i.GetName() << endl;
 
